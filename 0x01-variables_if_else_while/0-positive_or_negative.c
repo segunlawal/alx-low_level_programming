3df18bfb1adf2f12c1ea2b355b4a3f07201a0311
segunlawal <lawalsegun72@gmail.com>
2022-12-09 20:15:44 -0800

    make executable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+#include <stdio.h>
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -9,12 +9,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n==0)
+	if (n == 0)
 		printf ("%d is zero\n", n);
-	else if (n> 0)
-	printf ("%d is positive\n", n);
+	else if (n > 0)
+		printf ("%d is positive\n", n);
 	else 
-	printf ("%d is negative\n", n);
+		printf ("%d is negative\n", n);
 	/* your code goes there */
 	return (0);
 }
