13617aac38a5e12afa19f87e0ebc8d8125ccff2b
segunlawal <lawalsegun72@gmail.com>
2023-01-04 15:28:16 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,3 +6,17 @@
 
 void jack_bauer(void)
 {
+	for (int h = 0; h < 24; h++)
+	{
+		for (int m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
