37d8b8b82b7f72e456800276125723b2e266d0b2
segunlawal <lawalsegun72@gmail.com>
2023-01-04 15:07:15 -0800

    add tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * Return: result of addition
+ * @a: first integer
+ * @b: second integer
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
