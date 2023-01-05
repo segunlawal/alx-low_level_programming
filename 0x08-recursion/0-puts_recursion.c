d94fac3b14c3b45be5a9e032959dc49a97584dfa
segunlawal <lawalsegun72@gmail.com>
2023-01-05 09:49:37 -0800

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be checked
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0)
+	{
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
