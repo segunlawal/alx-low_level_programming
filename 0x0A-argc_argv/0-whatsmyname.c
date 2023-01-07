a920e2c8bb633704ec6e53a0ffa068d021b71208
segunlawal <lawalsegun72@gmail.com>
2023-01-07 04:23:14 -0800

    add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints its name
+ * argc: argument count
+ * argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s, argv[0]");
+	printf("\n");
+}
