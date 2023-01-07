a219e7fe5747f9571ebaf7ffcf527876ba914fdb
segunlawal <lawalsegun72@gmail.com>
2023-01-07 04:43:04 -0800

    add task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	if (argc > 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	printf("0\n");
+	return (0);
