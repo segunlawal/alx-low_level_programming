dfa2331e9c9b4fe1938da64f4256b8729528a416
segunlawal <lawalsegun72@gmail.com>
2023-01-09 11:58:48 -0800

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+/**
+ * _strlen - calculates length of a string
+ * @str: string
+ *
+ * Return: length of string
+ */
+
+unsigned int _strlen(char *str)
+{
+	unsigned int a;
+
+	for (a = 1; *str != '\0'; a++)
+		str++;
+	return (a);
+}
+
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: pointer to a newly allocated space in memory or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *str_dup;
+	unsigned int a, length;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+	length = _strlen(str);
+	str_dup = malloc(length * sizeof(char));
+	if (str_dup == NULL)
+		return (NULL);
+	for (a = 0; a < length; a++)
+		str_dup[a] = str[a];
+	str_cpy[a + 1] = '\0';
+	return (str_dup);
+	}
+}
