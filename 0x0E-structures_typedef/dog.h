ccd6ede3c4b2b679507518f110925a9389ca5228
segunlawal <lawalsegun72@gmail.com>
2023-01-16 09:37:03 -0800

    add task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif/*_DOG_H_*/
