55c96d8782509a1a5933ba1f72772f2199c6b8f8
segunlawal <lawalsegun72@gmail.com>
2023-01-16 10:02:14 -0800

    add task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,5 +12,8 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner)
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif/*_DOG_H_*/
