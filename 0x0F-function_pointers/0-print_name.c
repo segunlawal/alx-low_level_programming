961e87337b292587e2ba8ba0e733dd5dc3227b61
segunlawal <lawalsegun72@gmail.com>
2023-01-18 09:32:10 -0800

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name - name
+ * f - function that prints name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
