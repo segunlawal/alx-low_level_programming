7c4c980e4378f446a62170968d7a54846d5355bf
segunlawal <lawalsegun72@gmail.com>
2023-01-18 09:55:06 -0800

    update task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,12 +5,16 @@
  * @array: array
  * @size: size of array
  * @action: pointer to function
+ * Return: Nothing
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
