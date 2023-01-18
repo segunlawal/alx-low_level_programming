2f5a6a5cd881cbd52283ab4604788f9dc5774663
segunlawal <lawalsegun72@gmail.com>
2023-01-18 15:15:52 -0800

    fix betty warnings

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,36 +1,61 @@
-#include <stdlib.h>
-#include "3-calc.h"
-
 /**
- * op_add - returns the sum of a and b
- * op_sub - returns the difference of a and b
- * op_mul: returns the product of a and b
- * op_div: returns the result of the division of a by b
- * op_mod: returns the remainder of the division of a by b
+ * op_add - adds two numbers
  * @a: first number
  * @b: second number
- * Return: sum, difference, product, division or remainder
+ *
+ * Return: sum of a and b
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of a and b
+ */
 
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: product of a and b
+ */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of the division of a by b
+ */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
+/**
+ * op_mod - modulus of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of the division of a by b
+ */
 
 int op_mod(int a, int b)
 {
