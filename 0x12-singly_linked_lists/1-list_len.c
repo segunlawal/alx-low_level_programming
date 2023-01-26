d81b4feaf50d656dce588f80ef30272d906ad8b7
segunlawal <lawalsegun72@gmail.com>
2023-01-26 14:24:49 -0800

    add task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_list - returns the number of elements in a linked list
+ * @h: list head
+ * Return: the number of elements in list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t num;
+
+	for (num = 0; h != NULL; num++)
+	{
+		h = h->next;
+	}
+	return (num);
+}
