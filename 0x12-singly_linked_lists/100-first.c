63ecd70d6840ca1d31f6dbebf1a1172658c9f0e5
segunlawal <lawalsegun72@gmail.com>
2023-01-26 20:31:57 -0800

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * execute_f - executes before main function
+ *
+ * Return: Nothing
+ */
+
+void execute_f(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
