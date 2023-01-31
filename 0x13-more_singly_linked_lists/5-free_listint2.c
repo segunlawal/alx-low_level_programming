16479d58816c78786d93ca933227087a91dc2d10
segunlawal <lawalsegun72@gmail.com>
2023-01-30 23:08:26 -0800

    update task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head != NULL)
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
 	{
-		temp = *head->next;
+		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
