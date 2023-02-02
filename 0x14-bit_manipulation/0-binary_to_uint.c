8f48fcc6a93e0712c7f68961297e4b6f68976793
segunlawal <lawalsegun72@gmail.com>
2023-02-02 00:13:52 -0800

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of the string
+ */
+
+int _strlen(const char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen(s + 1));
+}
+
+/**
+ * _pow - returns the value of x raised to the power of y
+ * Return: value of x raised to the power of y
+ * @x: base number
+ * @y: exponent
+ */
+
+int _pow(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow(x, (y - 1)));
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b:  string of 0 and 1 chars
+ * Return: the converted number, or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int a, len, i;
+	unsigned int each_int;
+	unsigned int total = 0;
+
+	if (b == NULL)
+		return (0);
+	
+	len = _strlen(b);
+	for( a = 0; a < len; a++)
+	{
+		if ((b[a] != '0') && (b[a] != '1'))
+			return (0);
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		each_int = (b[len - i - 1] - '0') * (_pow(2, len - i - 1));
+		total += each_int;
+	}
+	return (total);
+
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,11 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 unsigned int binary_to_uint(const char *b);
+int _strlen(const char *s);
+int _pow(int x, int y);
 
 #endif/*_MAIN_H_*/
