b41438db957c7154b53ebb8dbd97f6ab72c1a57b
segunlawal <lawalsegun72@gmail.com>
2023-02-02 12:55:04 -0800

    task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,7 @@
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *s);
 int _pow(int x, int y);
+void print_binary(unsigned long int n);
+int _putchar(char c);
 
 #endif/*_MAIN_H_*/
