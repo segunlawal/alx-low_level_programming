26f51c3136d78819b7e0b0cd943f77840124cf10
segunlawal <lawalsegun72@gmail.com>
2023-02-02 20:33:13 -0800

    task 3

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ int _pow(int x, int y);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif/*_MAIN_H_*/
