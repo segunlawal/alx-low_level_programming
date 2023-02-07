9a1b24a89bd54adf18e6a2ca16a0bccff7d42d0e
segunlawal <lawalsegun72@gmail.com>
2023-02-06 18:31:19 -0800

    betty fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/*
+/**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of text file
  * @letters: the number of letters the function should read and print
