32211b118667494ca249302dc551879c9c12c218
segunlawal <lawalsegun72@gmail.com>
2023-02-06 18:17:53 -0800

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1 || filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	len = str_len(text_content);
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
